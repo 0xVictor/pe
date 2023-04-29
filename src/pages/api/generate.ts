a2c38407d6b7b4c690fb17330a8d9ce50629a424
João Víctor Carvalho <j.victor11@live.com>
2023-04-29 14:22:02 +0000

    Update 9 files

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -1,25 +1,47 @@
-import { Configuration, OpenAIApi } from 'openai';
+import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
-const basePromptPrefix = 'summarize the sentence: ';
+let basePromptPrefix = "summarize the sentence: ";
 const generateAction = async (req, res) => {
   // run first prompt
-  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+  console.log(
+    `API: ${basePromptPrefix}${req.body.userInput}, ${req.body.command}`
+  );
+  let maxTokens = "250";
+  switch (req.body.command) {
+    case "rewrite":
+      maxTokens = 500;
+      basePromptPrefix = "rewrite the sentence: ";
+      break;
+    case "summarize":
+      maxTokens = 300;
+      basePromptPrefix = "summarize the sentence: ";
+      break;
+    case "shorter":
+      maxTokens = 250;
+      basePromptPrefix = "make the the following sentence short: ";
+      break;
+    case "longer":
+      maxTokens = 1500;
+      basePromptPrefix = "make the the following sentence longer: ";
+      break;
+  }
 
+  console.log(maxTokens, basePromptPrefix, "dados");
   const baseCompletion = await openai.createCompletion({
-    model: 'text-davinci-003',
+    model: "text-davinci-003",
     prompt: `${basePromptPrefix}${req.body.userInput} `,
     temperature: 1,
-    max_tokens: 500,
+    max_tokens: maxTokens,
   });
 
-  const basePromptOutput = baseCompletion.data.choices.pop()
+  const basePromptOutput = baseCompletion.data.choices.pop();
 
   res.status(200).json({ output: basePromptOutput });
-}
+};
 
 export default generateAction;
---
+++ src/pages/editor.tsx
@@ -0,0 +1,6 @@
+import "@blocknote/core/style.css";
+import PersoEditor from "@/components/PersoEditor";
+
+export default function Home() {
+  return <PersoEditor />;
+}
